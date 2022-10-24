6cd4fec8fe9bda94d9ea633528b8003e1b8427e3
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-10-24 17:05:18 +0500

    register page added

---
+++ protected/views/auth/register.php
@@ -0,0 +1,31 @@
+<h1>
+    Registration
+</h1>
+
+<div class="form">
+    
+    <?php echo CHtml::beginForm(); ?>
+    <?php echo CHtml::errorSummary($model); ?>
+    
+    <div class="row">
+        <?php echo CHtml::activeLabel($model, 'Username'); ?>
+        <?php echo CHtml::activeTextField($model, 'username'); ?>
+    </div>
+    
+    <div class="row">
+        <?php echo CHtml::activeLabel($model, 'Password'); ?>
+        <?php echo CHtml::activeTextField($model, 'password'); ?>
+    </div>
+    
+    <div class="row">
+        <?php echo CHtml::activeCheckBox($model, 'rememberMe'); ?>
+        <?php echo CHtml::activeLabel($model, 'Remember Me'); ?>
+    </div>
+    
+    <div class="row submit">
+        <?php echo CHtml::submitButton('Register'); ?>
+    </div>
+    
+    <?php CHtml::endForm(); ?>
+    
+</div>
