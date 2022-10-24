580e8f6c3b28626d5e73adf5d55dfc9b2900005c
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-10-24 17:27:07 +0500

    beforeSave added

--- protected/models/User.php
+++ protected/models/User.php
@@ -15,65 +15,63 @@
  * The followings are the available model relations:
  * @property Link[] $links
  */
-class User extends CActiveRecord
-{	
-	public function tableName()
-	{
-		return 'user';
-	}
-	
-	public function rules()
-	{		
-		return array(
-			array('username, password, role, last_activity, create_time, update_time', 'required'),
-			array('username', 'length', 'max'=>60),
-			array('password', 'length', 'max'=>255),
-			array('role', 'length', 'max'=>45),
-			
-			array('id, username, password, role, last_activity, create_time, update_time', 'safe', 'on'=>'search'),
-		);
-	}
+class User extends CActiveRecord {
 
-	public function relations()
-	{		
-		return array(
-			'links' => array(self::HAS_MANY, 'Link', 'user_id'),
-		);
-	}
-	
-	public function attributeLabels()
-	{
-		return array(
-                        			'id' => Yii::t("translation", "id"),
-                        			'username' => Yii::t("translation", "username"),
-                        			'password' => Yii::t("translation", "password"),
-                        			'role' => Yii::t("translation", "role"),
-                        			'last_activity' => Yii::t("translation", "last_activity"),
-                        			'create_time' => Yii::t("translation", "create_time"),
-                        			'update_time' => Yii::t("translation", "update_time"),
-		);
-	}
-	
-	public function search()
-	{		
-		$criteria=new CDbCriteria;
+    public function tableName() {
+        return 'user';
+    }
 
-		$criteria->compare('id',$this->id);
-		$criteria->compare('username',$this->username,true);
-		$criteria->compare('password',$this->password,true);
-		$criteria->compare('role',$this->role,true);
-		$criteria->compare('last_activity',$this->last_activity,true);
-		$criteria->compare('create_time',$this->create_time,true);
-		$criteria->compare('update_time',$this->update_time,true);
+    public function rules() {
+        return array(
+            array('username, password, role, last_activity, create_time, update_time', 'required'),
+            array('username', 'length', 'max' => 60),
+            array('password', 'length', 'max' => 255),
+            array('role', 'length', 'max' => 45),
+            array('id, username, password, role, last_activity, create_time, update_time', 'safe', 'on' => 'search'),
+        );
+    }
 
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
+    public function relations() {
+        return array(
+            'links' => array(self::HAS_MANY, 'Link', 'user_id'),
+        );
+    }
 
-	
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+    public function attributeLabels() {
+        return array(
+            'id' => Yii::t("translation", "id"),
+            'username' => Yii::t("translation", "username"),
+            'password' => Yii::t("translation", "password"),
+            'role' => Yii::t("translation", "role"),
+            'last_activity' => Yii::t("translation", "last_activity"),
+            'create_time' => Yii::t("translation", "create_time"),
+            'update_time' => Yii::t("translation", "update_time"),
+        );
+    }
+
+    public function search() {
+        $criteria = new CDbCriteria;
+
+        $criteria->compare('id', $this->id);
+        $criteria->compare('username', $this->username, true);
+        $criteria->compare('password', $this->password, true);
+        $criteria->compare('role', $this->role, true);
+        $criteria->compare('last_activity', $this->last_activity, true);
+        $criteria->compare('create_time', $this->create_time, true);
+        $criteria->compare('update_time', $this->update_time, true);
+
+        return new CActiveDataProvider($this, array(
+            'criteria' => $criteria,
+        ));
+    }
+
+    public static function model($className = __CLASS__) {
+        return parent::model($className);
+    }
+
+    public function beforeSave() {
+        $this->password = md5($this->password);
+        return parent::beforeSave();
+    }
+
 }
