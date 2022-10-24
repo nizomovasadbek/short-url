666c3cb1919502afb73d774cabab11709263fb89
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-10-24 17:26:08 +0500

    Login and Logout added

--- protected/controllers/AuthController.php
+++ protected/controllers/AuthController.php
@@ -2,10 +2,24 @@
 
 class AuthController extends Controller {
 
+    private $iden;
+
     public function actionIndex() {
         if (!Yii::app()->user->isGuest) {
             Yii::app()->user->changeLastActivity();
         }
+        $this->redirect('/');
+        Yii::app()->end();
+    }
+
+    public function actionLogout() {
+        if (Yii::app()->user->isGuest) {
+            $this->redirect('/');
+            Yii::app()->end();
+        }
+
+        Yii::app()->user->changeLastActivity();
+        Yii::app()->user->logout();
         $this->redirect('/');
         Yii::app()->end();
     }
@@ -20,7 +34,21 @@ class AuthController extends Controller {
         $model = new LoginForm();
         $user = new User();
 
-        //TODO
+        if (isset($_POST['LoginForm'])) {
+            $model->attributes = $_POST['LoginForm'];
+            if ($model->validate()) {
+                $user->username = $model->username;
+                $user->password = md5($model->password);
+                $this->iden = new UserIdentity($user->username, $user->password);
+                if ($this->iden->authenticate()) {
+                    Yii::app()->user->login($this->iden);
+                    $user = User::model()->findByPk(Yii::app()->user->id);
+                    Yii::app()->user->changeLastActivity();
+                }
+                $this->redirect('/');
+                Yii::app()->end();
+            }
+        }
 
         $this->render('login', ['model' => $model]);
         Yii::app()->end();
@@ -36,7 +64,23 @@ class AuthController extends Controller {
         $model = new RegisterForm();
         $user = new User();
 
-        // TODO
+        if (isset($_POST['RegisterForm'])) {
+            $model->attributes = $_POST['RegisterForm'];
+            if ($model->validate()) {
+                $user->username = $model->username;
+                $user->password = $model->password;
+                $user->role = 'user';
+                $user->last_activity = $user->create_time = $user->update_time = date('Y-m-d H:i:s');
+                if ($user->save()) {
+                    $this->iden = new UserIdentity($user->username, $user->password);
+                    if ($this->iden->authenticate()) {
+                        Yii::app()->user->login($this->iden);
+                    }
+                    $this->redirect('/');
+                    Yii::app()->end();
+                }
+            }
+        }
 
         $this->render('register', ['model' => $model]);
         Yii::app()->end();
