f34a72608145194c8eb015a2a1b0cf580e27cac9
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-10-24 18:19:13 +0500

    Admin panel initialized

---
+++ protected/controllers/AdminController.php
@@ -0,0 +1,30 @@
+<?php
+
+class AdminController extends Controller {
+
+    public function filters() {
+        return array(
+            'accessControl'
+        );
+    }
+
+    public function accessRules() {
+        return array(
+            array(
+                'allow',
+                'actions' => array('index'),
+                'roles' => array('admin')
+            ),
+            array('deny',
+                'users' => array('*')
+            )
+        );
+    }
+
+    public function actionIndex() {
+        $users = User::model()->findAll();
+        $this->render('index', ['users' => $users]);
+        Yii::app()->end();
+    }
+
+}
