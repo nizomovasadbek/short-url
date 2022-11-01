b69ed91204ec7846f72106d3e45932ffdf8f9add
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-11-01 15:50:57 +0500

    Update action added

--- protected/controllers/AdminController.php
+++ protected/controllers/AdminController.php
@@ -11,7 +11,7 @@ class AdminController extends Controller {
     public function accessRules() {
         return [
                 ['allow',
-                'actions' => ['index', 'delete'],
+                'actions' => ['index', 'delete', 'update'],
                 'roles' => ['admin']
             ],
                 ['deny',
@@ -28,14 +28,37 @@ class AdminController extends Controller {
     }
 
     public function actionDelete($id) {
-        $user = User::model()->findByPk(Yii::app()->user->id);
+        $user = User::model()->findByPk($id);
         $criteria = new CDbCriteria();
         $criteria->compare('user_id', $user->id);
         $link = Link::model()->findAll($criteria);
-        foreach($link as $l){
+        foreach ($link as $l) {
             $l->delete();
         }
         $user->delete();
+    }
+
+    public function actionUpdate($id) {
+        $user = User::model()->findByPk($id);
+        $model = new UpdateUserForm();
+
+        if (isset($_POST['UpdateUserForm'])) {
+            $model->attributes = $_POST['UpdateUserForm'];
+            if ($model->validate()) {
+                if ($model->role == 1) {
+                    $user->role = 'admin';
+                } else if ($model->role == 2) {
+                    $user->role = 'user';
+                }
+                $user->update_time = date('Y-m-d H:i:s');
+            }
+            $user->saveAttributes(['role', 'update_time']);
+            $this->redirect('/admin');
+            Yii::app()->end();
+        }
+
+        $this->render('update', ['model' => $model]);
+        Yii::app()->end();
     }
 
 }
