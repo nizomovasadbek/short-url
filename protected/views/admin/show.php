41b6297622b3b2f598c386e3f4c38883c90c18ad
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-11-02 16:03:59 +0500

    Show action added feature

--- protected/views/admin/show.php
+++ protected/views/admin/show.php
@@ -1,2 +1,7 @@
-<a href="/admin/update/<?php echo $user->id; ?>">Update <?php echo $user->username; ?></a>
+<a href="/admin/update/<?php echo $user->id; ?>">Update <?php echo $user->username; ?></a><br>
 <a href="/admin/delete/<?php echo $user->id; ?>">Delete <?php echo $user->username; ?></a>
+<p>
+    <?php foreach($links as $link) {
+        echo "$link->short_url -------> $link->url<br>";
+    } ?>
+</p>
