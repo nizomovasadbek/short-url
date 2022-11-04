00b40adc75f2fbc8f007b261ba138bd1a201b092
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-11-04 16:28:46 +0500

    Bug fixed

--- protected/views/admin/update.php
+++ protected/views/admin/update.php
@@ -1,4 +1,4 @@
-<h1><?php echo Yii::t() . ' ' . $user->username . ' ' . Yii::t('translation', 'role'); ?></h1>
+<h1><?php echo Yii::t('translation', 'update') . ' ' . $user->username . ' ' . Yii::t('translation', 'role'); ?></h1>
 <div class="form">
 
     <?php echo CHtml::beginForm(); ?>
